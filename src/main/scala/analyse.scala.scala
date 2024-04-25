b43998347168d56d366e7ac5569ae4b52e34f99b
Javklin <allvis.gautier@gmail.com>
2024-04-25 16:36:06 +0200

    nombre de mot, phrase et livre fait

--- src/main/scala/analyse.scala.scala
+++ src/main/scala/analyse.scala.scala
@@ -8,20 +8,25 @@ object  Analyse {
 // retourne true si le df contient bien les bonnes informations false sinon
 def check_content(df: org.apache.spark.sql.DataFrame ) :Boolean = {
     val complete_data =false
+
     //nb de mot
+    val dfWithWordCount = df.withColumn("word_count", size(array_remove(split(col("book"), "\\s+"), "")))
+    dfWithWordCount.show(false)
+    val totalWordCount = dfWithWordCount.agg(sum("word_count")).collect()(0)(0)
+    println(s"Total number of words: $totalWordCount *******************************************************************************")
 
     //nb de phrase 
     val countSentencesUDF = udf((text: String) => {
     text.split("[.!?]").count(_.trim.nonEmpty)
-    })    
+    })
     val dfWithSentenceCount = df.withColumn("sentence_count", countSentencesUDF(col("book")))
-    //dfWithSentenceCount.show()
+    dfWithSentenceCount.show(false)
     val totalSentenceCount = dfWithSentenceCount.agg(sum("sentence_count")).collect()(0)(0)
-    //println(s"Total number of sentences: $totalSentenceCount")
+    println(s"Total number of sentences: $totalSentenceCount *******************************************************************************")
 
     //nb de livre
     val book_number =df.count()
-    println(s"Total number of book: $book_number")
+    println(s"Total number of book: $book_number *******************************************************************************")
     val different_word_number=0
     val average_word_by_sentence = 0
     val median_word_by_sentence = 0
