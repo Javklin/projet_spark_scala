45048aa193cb2663abeb3be0c03fc9777ad52c1d
Javklin <allvis.gautier@gmail.com>
2024-04-25 20:14:05 +0200

    partie analyse termine

--- src/main/scala/analyse.scala.scala
+++ src/main/scala/analyse.scala.scala
@@ -10,26 +10,46 @@ def check_content(df: org.apache.spark.sql.DataFrame ) :Boolean = {
     val complete_data =false
 
     //nb de mot
-    val dfWithWordCount = df.withColumn("word_count", size(array_remove(split(col("book"), "\\s+"), "")))
-    dfWithWordCount.show(false)
-    val totalWordCount = dfWithWordCount.agg(sum("word_count")).collect()(0)(0)
-    println(s"Total number of words: $totalWordCount *******************************************************************************")
+    val df_with_word_count = df.withColumn("word_count", size(array_remove(split(col("book"), "\\s+"), "")))
+    df_with_word_count.show(false)
+    val total_word_count = df_with_word_count.agg(sum("word_count")).collect()(0)(0)
+    //println(s"Total number of words: $total_word_count *******************************************************************************")
 
     //nb de phrase 
-    val countSentencesUDF = udf((text: String) => {
+    val count_sentence_udf = udf((text: String) => {
     text.split("[.!?]").count(_.trim.nonEmpty)
     })
-    val dfWithSentenceCount = df.withColumn("sentence_count", countSentencesUDF(col("book")))
-    dfWithSentenceCount.show(false)
-    val totalSentenceCount = dfWithSentenceCount.agg(sum("sentence_count")).collect()(0)(0)
-    println(s"Total number of sentences: $totalSentenceCount *******************************************************************************")
+    val df_with_sentence_count = df.withColumn("sentence_count", count_sentence_udf(col("book")))
+    df_with_sentence_count.show(false)
+    val totalSentenceCount = df_with_sentence_count.filter(trim(col("book")) =!= "").agg(sum("sentence_count")).collect()(0)(0)
+    //println(s"Total number of sentences: $totalSentenceCount *******************************************************************************")
 
     //nb de livre
     val book_number =df.count()
+    //println(s"Total number of book: $book_number *******************************************************************************")
+    
+    // nb de mot different
+    // val wordsDF = df.withColumn("word", explode(split(col("book"), "\\s+")))
+    val words_df = df.withColumn("word", explode(split(col("book"), "\\s+")))
+    .withColumn("word", regexp_replace(col("word"), "[\\.\\!\\?]", ""))
+    val distinct_word_count_df = words_df.filter(trim(col("word")) =!= "").groupBy("word").count()
+    distinct_word_count_df.show()
+    val different_word_number_df = distinct_word_count_df.agg(sum("count").alias("total_count"))
+    val different_word_number = distinct_word_count_df.count()
+
+    // la moyenne de mot par phrase
+    val average_word_by_sentence = df_with_sentence_count
+    .agg(sum("sentence_count")).count()
+
+     // la median de mot par phrase
+    val median_word_by_sentence = df_with_sentence_count.stat.approxQuantile("sentence_count", Array(0.5), 0.0)(0)
+
+    println(s"Total number of words: $total_word_count *******************************************************************************")
+    println(s"Total number of sentences: $totalSentenceCount *******************************************************************************")
     println(s"Total number of book: $book_number *******************************************************************************")
-    val different_word_number=0
-    val average_word_by_sentence = 0
-    val median_word_by_sentence = 0
+    println(s"Distinct number of word: $different_word_number *******************************************************************************")
+    println(s"average of words by sentences: $average_word_by_sentence  *******************************************************************************")
+    println(s"median of words by sentences: $median_word_by_sentence *******************************************************************************")
     return complete_data
 }
 
